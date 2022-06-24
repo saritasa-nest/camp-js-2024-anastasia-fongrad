9f5e7c0d4e303dac6a120c14cc613a6943b306fe
maxineo <korkinwow@gmail.com>
2022-06-24 12:49:57 +0700

    Added button-has-type rule for angular eslint config #FT-28

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SharedModule } from './../shared/shared.module';
-import { AppRoutingModule } from './app-routing-module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 /** App module. */
