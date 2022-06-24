1b33773480f486b360edc77310cda7e3eb1dd40c
maxineo <korkinwow@gmail.com>
2022-06-24 12:34:44 +0700

    Changed angular prefix #FT-28

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 /** App component. */
 @Component({
-  selector: 'saritasa-root',
+  selector: 'camp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
