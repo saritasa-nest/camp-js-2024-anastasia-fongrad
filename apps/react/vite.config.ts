88e6b2e284841f0d633737a2833c143b030704c7
dmitriqm <1dmitriqm@gmail.com>
2022-09-12 14:02:45 +0500

    Add eslint optimization for react project

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -2,9 +2,11 @@
 import path from 'path';
 
 import { defineConfig } from 'vite';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [eslintPlugin({ eslintOptions: { cache: false } })],
   resolve: {
     alias: {
       '@js-camp/react': path.resolve(__dirname, 'src'),
