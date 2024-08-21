f1e0f367e03baf3e3e351a6da1559cab408d6ae5
datngovan <datngo.qlct@gmail.com>
2024-08-21 20:58:36 +0700

    Add multiple selected selector for genres filter JC-713

--- apps/react/src/features/genres/components/GenresFilter/GenresFilter.tsx
+++ apps/react/src/features/genres/components/GenresFilter/GenresFilter.tsx
@@ -1,51 +1,44 @@
-import { memo, FC, useState, ChangeEvent, useEffect } from "react";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import { Box, Typography } from "@mui/material";
+import { memo, FC, useState } from "react";
+import { Select, MenuItem, FormControl, InputLabel, Checkbox, ListItemText, SelectChangeEvent } from "@mui/material";
+import { GenresQueryParams } from "@js-camp/react/model/genres-query-params.model";
 
-import styles from "./GenresFilter.module.css";
-import useQueryParams from "../../hooks/useQueryParams";
-
+const filters = [
+	{ value: "", label: "Default" },
+	{ value: GenresQueryParams.FilterType.Genres, label: "Genres" },
+	{ value: GenresQueryParams.FilterType.ExplicitGenres, label: "Explicit genres" },
+	{ value: GenresQueryParams.FilterType.Themes, label: "Themes" },
+	{ value: GenresQueryParams.FilterType.Demographics, label: "Demographics" },
+];
 const GenresFilterComponent: FC = () => {
-	const { getQueryParamByKey, setQueryParams } = useQueryParams();
-	const searchParams = getQueryParamByKey("search");
-	const [queryParamsValue, setQueryParamsValue] = useState(searchParams || "");
+	const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
 
-	/**
-	 * Handle query params value.
-	 * @param event Input change event.
-	 */
-	function handleQueryParamsValue(event: ChangeEvent<HTMLInputElement>): void {
-		event.preventDefault();
-		setQueryParamsValue(event.target.value);
-	}
-
-	// Update the query params whenever the input value changes
-	useEffect(() => {
-		const searchValue = queryParamsValue.length > 0 ? queryParamsValue : null;
-		setQueryParams({ search: searchValue });
-	}, [queryParamsValue]);
+	const handleChange = (event: SelectChangeEvent<string[]>) => {
+		const { value } = event.target;
+		const newSelectedFilters = typeof value === "string" ? value.split(",") : value;
+		setSelectedFilters(newSelectedFilters);
+		console.log("Selected filters:", newSelectedFilters);
+	};
 
 	return (
-		<Box className={styles.filters}>
-			<Typography variant="h5" component="h5" gutterBottom>
-				Filters
-			</Typography>
-			<Paper component="form" className={styles.filters__form}>
-				<InputBase
-					className={styles["filters__input-base"]}
-					placeholder="Search Genres ..."
-					aria-label="search genres"
-					value={queryParamsValue}
-					onChange={handleQueryParamsValue}
-				/>
-				<IconButton type="button" className={styles["filters__icon-button"]} aria-label="search" color="primary">
-					<SearchIcon />
-				</IconButton>
-			</Paper>
-		</Box>
+		<FormControl fullWidth>
+			<InputLabel id="multiple-filters-select-label">Select Filters</InputLabel>
+			<Select
+				labelId="multiple-filters-select-label"
+				multiple
+				value={selectedFilters}
+				onChange={handleChange}
+				renderValue={(selected) =>
+					selected.map((val) => filters.find((filter) => filter.value === val)?.label).join(", ")
+				}
+			>
+				{filters.map((filter) => (
+					<MenuItem key={filter.value} value={filter.value}>
+						<Checkbox checked={selectedFilters.indexOf(filter.value) > -1} />
+						<ListItemText primary={filter.label} />
+					</MenuItem>
+				))}
+			</Select>
+		</FormControl>
 	);
 };
 
