ac0f891dbda8c7f288242d087a1f27484b18e743
Anastasia Fongrad <anastasia.fongrad@saritasa.com>
2024-08-22 10:01:49 +0700

    Create genres select, start implemented anime dialogs JC-706

---
+++ apps/angular/src/app/features/anime-management/studios-dialog/studios-dialog.component.ts
@@ -0,0 +1,56 @@
+import { Component, inject, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {
+	MAT_DIALOG_DATA,
+	MatDialogActions,
+	MatDialogClose,
+	MatDialogContent,
+	MatDialogRef,
+	MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+/** 1. */
+export type DialogData = {
+
+	/** 1. */
+	readonly animal: string;
+
+	/** 1. */
+	readonly name: string;
+};
+
+/** 1. */
+@Component({
+	selector: 'camp-studios-dialog',
+	templateUrl: './studios-dialog.component.html',
+	standalone: true,
+	imports: [
+		MatFormFieldModule,
+		MatInputModule,
+		FormsModule,
+		MatButtonModule,
+		MatDialogTitle,
+		MatDialogContent,
+		MatDialogActions,
+		MatDialogClose,
+	],
+})
+export class StudiosDialogComponent {
+
+	/** 1. */
+	public readonly dialogRef = inject(MatDialogRef<StudiosDialogComponent>);
+
+	/** 1. */
+	protected readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+
+	/** 1. */
+	protected readonly animal = model(this.data.animal);
+
+	/** 1. */
+	protected onNoClick(): void {
+		this.dialogRef.close();
+	}
+}
