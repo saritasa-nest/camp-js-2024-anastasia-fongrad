2b0daaa5ed7e4119db959de7ae4348cf107e8f6f
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-08-26 10:22:26 +0500

    Remove unused function JC-716

--- apps/react/src/features/studios/utils/objectToQueryString.ts
+++
@@ -1,9 +0,0 @@
-import { StudioQueryParameters } from '@js-camp/core/models/studio-query-parameters.model';
-
-/**
- * Transforms object to query string.
- * @param options Url params in object format.
- */
-export function objectToQueryString(options: StudioQueryParameters): string {
-	return new URLSearchParams(JSON.parse(JSON.stringify(options))).toString();
-}
